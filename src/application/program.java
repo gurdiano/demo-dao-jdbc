9e2936fa9c9ba858a278f45e3d29a3fec19a5cb4
Tiago Gurdiano <tiagofran2013@hotmail.com>
2022-09-23 11:25:42 -0300

    Department entity class

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "Books");
+		
+		System.out.println(dep);
+
+	}
+
+}
